930b8ab9223aea7ec84a85cb3b151d91f87c4e7c
jdgwartney <davidg@boundary.com>
2014-09-05 15:50:09 -0700

    Stub out java plugin

---
+++ src/main/java/com/boundary/metric/plugin/jmx/Plugin.java
@@ -0,0 +1,12 @@
+package com.boundary.metric.plugin.jmx;
+
+public class Plugin {
+  
+  public Plugin() {
+  }
+
+  public static void main(String args) {
+     System.out.println("JMX_ATTRIBUTE 100 localhost");
+  }
+
+}
