fd0ac553f004d4ada6f367b559087c17773a2459
jdgwartney <davidg@boundary.com>
2014-09-05 17:54:20 -0700

    Add resolving hostname

--- src/main/java/com/boundary/metric/plugin/jmx/Plugin.java
+++ src/main/java/com/boundary/metric/plugin/jmx/Plugin.java
@@ -1,5 +1,7 @@
 package com.boundary.metric.plugin.jmx;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Random;
 
 public class Plugin {
@@ -7,13 +9,14 @@ public class Plugin {
 	public Plugin() {
 	}
 
-	public static void main(String args[]) throws InterruptedException {
+	public static void main(String args[]) throws InterruptedException, UnknownHostException {
 
 		while (true) {
 
 			int random = new Random().nextInt(100);
+			String hostname = InetAddress.getLocalHost().getHostName();
 
-			System.out.println("JMX_ATTRIBUTE " + random + " localhost");
+			System.out.println("JMX_ATTRIBUTE " + random + " " + hostname);
 			Thread.sleep(5000);
 		}
 	}
